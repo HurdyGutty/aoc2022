3f86cf94b864f9a1d3a807304827715b4446e0d4
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2022-12-06 14:59:51 +0700

    Work PC upload

---
+++ day6_1.js
@@ -0,0 +1,21 @@
+const {readFileSync, promises: fsPromises} = require('fs');
+    const file = './message.txt';
+
+    function syncReadFile(filename) {
+            const contents = readFileSync(filename, 'utf-8');
+        
+            return contents
+          
+    }
+
+    const text = syncReadFile(file);
+    let length = text.length;
+
+for (i = 4, j = 0; i < length; i++, j++){
+    let buffer_code = text.substring(j,i);
+    let buffer_different = new Set(buffer_code).size;
+    if (buffer_different == 4){
+        console.log(i);
+        break;
+    }
+}
