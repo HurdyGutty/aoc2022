8409467721a8b58a3884ce9d699eb3c8dcfebed3
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2022-12-20 16:52:09 +0700

    Work PC upload day 7 part 1

--- day7_1.js
+++ day7_1.js
@@ -12,15 +12,15 @@ const {readFileSync, promises: fsPromises} = require('fs');
 
     const arr_read = syncReadFile(file);
 
-    const root_regex = /(?:\$ cd )\//g;
+    const root_regex = /(?:\$ cd )\//;
 
-    const folder_regex = /(?:\$ cd )(\w+)/g;
+    const folder_regex = /(?:\$ cd )(\w+)/;
 
-    const child_regex = /(?:dir )(\w+)/g;
+    const child_regex = /(?:dir )(\w+)/;
 
-    const file_regex = /(\d+)(?: \w+.\w+)/g;
+    const file_regex = /(\d+)(?: \w+.?\w*)/;
 
-    const cdline_regex = /\$ cd .+/;
+    const cdline_regex = /\$ cd \.+/;
 
     let current_path = [];
 
@@ -29,14 +29,14 @@ const {readFileSync, promises: fsPromises} = require('fs');
     function current_directory(directory, object_map = new Map([['sum', 0]])){
         if (directory === '..'){
             current_path.pop();
+            return;
         }
         current_path.push(object_map);
     }
 
     for (let i = 0; i < arr_read.length; i++){
-
         // $ cd /
-        if (arr_read[i].search(root_regex) !== -1){
+        if (root_regex.test(arr_read[i]) === true){
             let directory = '/';
             let folder_name = 'root';
             let root = new Map([['sum', 0]]);
@@ -44,34 +44,46 @@ const {readFileSync, promises: fsPromises} = require('fs');
             folder.set(folder_name, root);
             current_directory(directory, root);
         }
-
         // $ cd dfghsdfgs
-        if (arr_read[i].search(folder_regex) !== -1){
+        if (folder_regex.test(arr_read[i]) === true){
             let directory = [...arr_read[i].matchAll(folder_regex)][0][1];
             let current = current_path[current_path.length - 1].get(directory);
             current_directory(directory, current);
         }
-
         // dir asdfasdfas
-        if (arr_read[i].search(child_regex) !== -1){
+        if (child_regex.test(arr_read[i]) === true){
             let folder_name = [...arr_read[i].matchAll(child_regex)][0][1];
-            let child = new Map([['sum', 0]]);
-            current_path[current_path.length - 1].set(folder_name,child);
+            let folder = new Map([['sum', 0]]);
+            current_path[current_path.length - 1].set(folder_name,folder);
         }
-
         // 132454 asdasdd.sfs
-        if (arr_read[i].search(file_regex) !== -1){
+        if (file_regex.test(arr_read[i]) === true){
             let file_size = [...arr_read[i].matchAll(file_regex)][0][1];
-            let current_directory = current_path[current_path.length - 1];
-            current_directory.set('sum', current_directory.get('sum') + Number(file_size));
+            current_path.forEach(el => el.set('sum', el.get('sum') + Number(file_size)));
         }
-
         // $ cd ..
-        if (arr_read[i].search(cdline_regex) !== -1){
+        if (cdline_regex.test(arr_read[i]) === true){
             current_directory('..')
         }
-
         if (arr_read[i] === '$ ls') continue;
     }
+    let sum_all = 0;
 
-    console.log(folder);
+    function folder_sum_recursive(folder){
+        if (folder.size == 1 && folder.has('sum')){
+            return folder.get('sum') <= 100000 ? folder.get('sum') : 0;
+        }
+        for (key of folder.keys()){
+            if (key != 'sum'){
+                sum_all += folder_sum_recursive(folder.get(key));
+            }
+            else{
+                continue;
+            }
+        }
+        return folder.get('sum') <= 100000 ? folder.get('sum') : 0;
+    }
+
+    folder_sum_recursive(folder);
+
+    console.log(folder_sum_recursive(folder));
